26db65f75ec09722e9234183c7843a165bf2c49d
khushal <khushal2004a@gmail.com>
2024-10-25 16:40:39 +0530

    added all files

---
+++ hooks/src/index.ts
@@ -0,0 +1,37 @@
+import express from "express"
+import { PrismaClient } from "@prisma/client"
+
+const client = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+  const userId = req.params.userId
+  const zapId = req.params.zapId
+
+  const body = req.body
+
+  await client.$transaction(async tx => {
+    const zapRun = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body
+      }
+    })
+
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: zapRun.id
+      }
+    })
+  })
+
+  res.json({
+    message: 'webhook received'
+  })
+})
+
+app.listen(3000, () => {
+  console.log("server running at port 3000")
+})
